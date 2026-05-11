2de0f2938b0038025039f1659c6b3efc9d6b4648
sanglevijayuk-coder <sangle.vijayuk@gmail.com>
2026-05-11 12:17:54 +0100

    Updated Hello Handler function.

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,8 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi, is everything working from your Laptop?";
-		return hello;
+		return "Hi, is everything working from your Laptop?";
 	}
 
 }
